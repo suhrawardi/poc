23c4bfb6979ef78daf2caece9b8df09830d784e1
jarra <suhrawardi@gmail.com>
2018-11-17 15:16:09 +0100

    A bit further

---
+++ src/Helpers.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE Arrows #-}
+
+module Helpers (
+    getDeviceIDs,
+    styling
+  ) where
+
+import Euterpea
+import HSoM
+import FRP.UISF
+
+
+getDeviceIDs = topDown $
+  proc () -> do
+    mi <- selectInput -< ()
+    mo <- selectOutput -< ()
+    outA -< (mi, mo)
+
+styling title (h, w) = defaultMUIParams {uiTitle = title, uiSize = (h, w)}
--- src/Two.hs
+++ src/Two.hs
@@ -5,8 +5,10 @@ module Two (
   ) where
 
 import Euterpea
+import Helpers
 import HSoM
 import FRP.UISF
+
 
 secondCounter :: UISF () ()
 secondCounter = proc () -> do
@@ -15,4 +17,22 @@ secondCounter = proc () -> do
   rec count <- fcdelay 0 1 -< count + 1
   display -< count
 
-rrun = runMUI' secondCounter
+ui5 :: UISF () ()
+ui5 = proc _ -> do
+  (_, devid) <- getDeviceIDs -< ()
+  ap <- title "Absolute Pitch" (hiSlider 1 (0, 100) 0) -< ()
+  f <- title "Tempo" (hSlider (1,10) 1) -< ()
+  title "Pitch" display -< pitch ap
+  tick <- timer -< 1/f
+  midiOut -< (devid, fmap(const [ANote 0 ap 100 0.1]) tick)
+
+
+ui3 :: UISF () ()
+ui3 = proc _ -> do
+  (_, mo) <- getDeviceIDs -< ()
+  ap <- title "Absolute Pitch" (hiSlider 1 (0, 100) 0) -< ()
+  title "Pitch" display -< pitch ap
+  uap <- unique -< ap
+  midiOut -< (mo, fmap (\k -> [ANote 0 k 100 0.1]) uap)
+
+rrun = runMUI (styling "Midi Out / In" (400, 400)) ui5
