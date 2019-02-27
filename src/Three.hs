f18fde534419f06feb74ef11dca7a35a1d1afb8d
jarra <suhrawardi@gmail.com>
2019-02-27 21:30:33 +0100

    Random notes

--- src/Three.hs
+++ src/Three.hs
@@ -4,20 +4,29 @@ module Three (
     runThree
   ) where
 
-import Data.Maybe --(catMaybes, mapMaybe)
-import Text.Read (readMaybe)
 import Euterpea
 import Helpers
 import HSoM
 import FRP.UISF
+import System.Random
+import System.Random.Distributions
 
 
-asNote :: Pitch -> [MidiMessage]
-asNote x = [ANote 0 n 64 0.05]
+asNote2 :: Pitch -> [MidiMessage]
+asNote2 x = [ANote 0 n 64 0.05]
             where n = absPitch x
 
-ps :: [Pitch]
-ps = [(C,4), (D,4), (E,4)]
+
+sGen :: StdGen
+sGen = mkStdGen 42
+
+
+asMidiMessage :: Int -> SEvent [MidiMessage]
+asMidiMessage p = Just [ANote 0 p 64 0.05]
+
+
+randFreq :: Double
+randFreq = head $ randomRs (1,10) sGen
 
 
 
@@ -26,12 +35,20 @@ threeUI = proc _ -> do
   (mi, mo) <- getDeviceIDs -< ()
   m <- midiIn -< mi
 
+  -- rf <- liftAIO randomRIO -< (0.1, 1.0)
+
   f <- title "Frequency" $ withDisplay (hSlider (0.1, 10) 0.1) -< ()
   tick <- timer -< 1/f
 
   m2 <- hold [ANote 0 1 64 0.05] -< m
   _ <- title "Midi in" display -< m2
 
-  midiOut -< (mo, fmap(const (asNote (C,4))) tick)
+  r1 <- liftAIO randomRIO -< (30, 70)
+  r2 <- liftAIO randomRIO -< (30, 70)
+
+  _ <- title "R1" display -< asMidiMessage r1
+  _ <- title "R2" display -< asMidiMessage r2
+
+  midiOut -< (mo, fmap const asMidiMessage r1 tick)
 
 runThree = runMUI (styling "Blah!" (300, 600)) threeUI
