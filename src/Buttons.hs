271ec14f09ce0426835c3a29014d46481296a9fc
jarra <suhrawardi@gmail.com>
2019-03-05 17:03:21 +0100

    Refactoring

--- src/Buttons.hs
+++ src/Buttons.hs
@@ -9,7 +9,7 @@ import Data.Maybe (isJust)
 import FRP.UISF
 
 
-buttonsPanel = topDown $ proc _ -> do
+buttonsPanel = title "Start/Stop" $ topDown $ proc _ -> do
     start <- edge <<< button "Start" -< ()
     stop <- edge <<< button "Stop" -< ()
     returnA -< (start, stop)
--- src/Three.hs
+++ src/Three.hs
@@ -26,19 +26,26 @@ throttle = proc (channel, freq, tick, isPlaying) -> do
     returnA -< messages
 
 
-randomPitchBound = proc (a, b) -> do
+randomPitchBound = title "Pitch" $ topDown $ proc (a, b) -> do
     min <- title "Min" $ withDisplay (hiSlider 1 (30, 70) 60) -< ()
     max <- title "Max" $ withDisplay (hiSlider 1 (30, 70) 60) -< ()
     returnA -< (min, max)
 
 
-randomFreq = proc _ -> do
+randomFreq = title "Rate" $ leftRight $ proc _ -> do
     max <- title "Max Rate" $ withDisplay (hSlider (0.01, 10.0) 1.0) -< ()
     fr <- title "Frequency" $ withDisplay (hSlider (0.01, 10.0) 0.1) -< ()
     rnd <- liftAIO randomRIO -< (0.1, max)
     rec frq <- delay 1.0 -< frq'
         let frq' = rnd / fr * 10
     returnA -< frq
+
+
+channelPanel = title "Channel" $ leftRight $ proc (a, b) -> do
+    (minP, maxP) <- randomPitchBound -< (a, b)
+    pitch <- liftAIO randomRIO -< (minP, maxP)
+    isPlaying <- buttonsPanel >>> handleButtons -< ()
+    returnA -< (pitch, isPlaying)
 
 
 threeUI :: UISF () ()
@@ -50,12 +57,7 @@ threeUI = proc _ -> do
   m2 <- hold [ANote 0 1 64 0.05] -< m
   _ <- title "Midi in" display -< m2
 
-  -- Set whether it's playing
-  (start, stop) <- buttonsPanel -< ()
-  isPlaying <- handleButtons -< (start, stop)
-
-  (minP, maxP) <- randomPitchBound -< (30, 70)
-  r1 <- liftAIO randomRIO -< (minP, maxP)
+  (r1, isPlaying) <- channelPanel -< (30, 70)
 
 
   frq <- randomFreq -< ()
