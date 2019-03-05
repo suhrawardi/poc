3ec44dcd724da7401fd11d6f492b3cac3c0de423
jarra <suhrawardi@gmail.com>
2019-03-05 20:22:50 +0100

    Added decay

--- src/Three.hs
+++ src/Three.hs
@@ -5,7 +5,7 @@ module Three (
   ) where
 
 import Buttons
-import Data.Maybe (isJust)
+import Data.Maybe (mapMaybe, isJust)
 import Euterpea
 import Helpers
 import HSoM
@@ -16,6 +16,22 @@ import System.Random.Distributions
 
 sGen :: StdGen
 sGen = mkStdGen 42
+
+
+decay :: Time -> Double -> MidiMessage -> Maybe MidiMessage
+decay dur r m =
+  let f c k v d = if v > 0
+                  then let v' = truncate(fromIntegral v * r)
+                       in Just (ANote c k v' d)
+                  else Nothing
+  in case m of
+    ANote c k v d      -> f c k v d
+    Std (NoteOn c k v) -> f c k v dur
+    _                  -> Nothing
+
+
+grow :: Double -> Double -> Double
+grow r x = r * x * (1 - x)
 
 
 throttle = proc (channel, freq, tick, isPlaying) -> do
@@ -39,14 +55,23 @@ channelPanel = title "Channel" $ leftRight $ proc (a, b) -> do
     returnA -< (pitch, isPlaying)
 
 
+delayPanel = title "Delay" $ leftRight $ proc m -> do
+    r <- title "Growth rate" $ withDisplay (hSlider (2.4, 4.0) 2.4) -< ()
+    d <- title "Decay rate" $ withDisplay (hSlider (0, 0.9) 0.1) -< ()
+    f <- title "Echo frequency" $ withDisplay (hSlider (0, 10) 0) -< ()
+    rec s <- vdelay -< (1/f, fmap (mapMaybe (decay 0.1 r)) m')
+        let m' = mappend m s
+    m2 <- hold [ANote 0 1 64 0.05] -< s
+    _ <- title "Midi in" display -< m2
+    returnA -< s
+
+
 threeUI :: UISF () ()
 threeUI = proc _ -> do
   (mi, mo) <- getDeviceIDs -< ()
   m <- midiIn -< mi
 
-  -- Display incoming Midi
-  m2 <- hold [ANote 0 1 64 0.05] -< m
-  _ <- title "Midi in" display -< m2
+  m2 <- delayPanel -< m
 
   (r1, isPlaying) <- channelPanel -< (30, 70)
 
@@ -54,9 +79,7 @@ threeUI = proc _ -> do
   tick <- timer -< 1 / f
   _ <- title "Tick" display -< tick
 
-
   outMsg <- throttle -< (0, r1, tick, isPlaying)
-
 
   midiOut -< (mo, outMsg)
 
